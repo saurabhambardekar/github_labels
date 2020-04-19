d7169112fae0b79ee85c65553426aa97e676655d
saurabhambardekar <sau.manu07@gmail.com>
2020-04-19 23:10:54 +0530

    Updates

---
+++ precache-manifest.e14dd8f577b273304321d0beeaf8152d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb23b47355d11b91a23d041c069f3079",
+    "url": "/github_labels/index.html"
+  },
+  {
+    "revision": "4b0aa7f97ccbeaa01451",
+    "url": "/github_labels/static/css/main.7ccaf96e.chunk.css"
+  },
+  {
+    "revision": "4b19a77846414ec1b010",
+    "url": "/github_labels/static/js/2.5a0b0547.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github_labels/static/js/2.5a0b0547.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b0aa7f97ccbeaa01451",
+    "url": "/github_labels/static/js/main.04a7500a.chunk.js"
+  },
+  {
+    "revision": "d39efd3c661fe85e05f2",
+    "url": "/github_labels/static/js/runtime-main.f585eb3c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github_labels/static/media/logo.5d5d9eef.svg"
+  }
+]);
